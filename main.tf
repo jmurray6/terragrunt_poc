cd1f5c96565015b01dba3ec8128cefc9c856fc7c
EC2 Default User <ec2-user@ip-172-31-33-84.ec2.internal>
2018-12-07 15:47:40 +0000

    initial deploy

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+	region = "us-east-1"
+	shared_credentials_file = "/home/ec2-user/.aws/credentials"
+	profile = "terraform"
+}
 
+resource "aws_instance"
+"web" {
+	ami = "ami-009d6802948d06e52"
+	instance_type = "t1.micro"
+	tags {
+		Name = "Test_Terraform_A"
+}
+}
