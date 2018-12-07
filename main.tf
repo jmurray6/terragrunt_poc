6eeaed0e22008da0f4389768f3638ea959d81fa7
EC2 Default User <ec2-user@ip-172-31-33-84.ec2.internal>
2018-12-07 16:38:55 +0000

    Push with vars.tfars update

--- main.tf
+++ main.tf
@@ -1,14 +1,23 @@
+variable "region" {}
+variable "profile" {}
+variable "instance_type" {}
+variable "my_ami" {
+	type = "map"
+}
+variable "key_name"  {}
+
 provider "aws" {
-	region = "us-east-1"
+	region = "${var.region}"
 	shared_credentials_file = "/home/ec2-user/.aws/credentials"
 	profile = "terraform"
 }
 
 resource "aws_instance"
 "web" {
-	ami = "ami-009d6802948d06e52"
+	ami = "${lookup(var.my_ami, var.region)}"
 	instance_type = "t1.micro"
 	tags {
 		Name = "Test_Terraform_A"
-}
+	}
+	key_name = "${var.key_name}"
 }
